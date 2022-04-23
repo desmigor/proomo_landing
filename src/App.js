895e4c7d1c877d6f16e3a5d50fe734a067c557bc
Igor M <mporeigor@gmail.com>
2022-04-23 18:34:21 +0300

    folders and files structure completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
 import './App.css';
+import { Header, WhatProomo, Blog, Features, Possibility, Footer } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 function App() {
   return (
-    <div>
-      hello
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatProomo />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/containers/whatProomo/WhatProomo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './whatProomo.css';
+
+function WhatProomo() {
+    return (
+        <div>WhatProomo</div>
+    )
+}
+
+export default WhatProomo
